389731eef77567f76ccd7cbb859fd2009fb0143c
Tien Dai - Unity Dev Game <daiht@AS.LOCAL>
2025-10-08 20:10:08 +0700

    voyer

---
+++ Assets/_TestAsset/Scripts/ConveyorController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geckout
+{
+    public class ConveyorController : SingletonMono<ConveyorController>
+    {
+        [SerializeField] private Transform[] wayPoints;
+
+        public Transform[] WayPoints { get => wayPoints; }
+    }
+}
---
+++ Assets/_TestAsset/Scripts/MoveAlongPath.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using Geckout;
+using UnityEngine;
+
+public class MoveAlongPath : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    public float duration = 5f;       
+    public bool loop = false;         
+    public bool lookForward = true;
+
+    void Start()
+    {
+        waypoints = ConveyorController.Instance.WayPoints;
+        Vector3[] path = new Vector3[waypoints.Length];
+        for (int i = 0; i < waypoints.Length; i++)
+            path[i] = waypoints[i].position;
+
+        var tween = transform.DOPath(path, duration, PathType.Linear, PathMode.Full3D)
+                             .SetEase(Ease.Linear).SetId(this);
+
+        if (lookForward)
+            tween.SetLookAt(0.01f);
+
+        if (loop)
+            tween.SetLoops(-1, LoopType.Restart);
+    }
+}
