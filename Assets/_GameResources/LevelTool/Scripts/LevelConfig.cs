4d96ffb2e737a529e52614949584efddd2a58915
tuandt <tuandt@android.vn>
2025-10-08 14:29:06 +0700

    Add level tool folder, update unity 2022.3.52f2

---
+++ Assets/_GameResources/LevelTool/Scripts/LevelConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geckout.Tools
+{
+    public class LevelConfig : MonoBehaviour
+    {
+        
+    }
+}
