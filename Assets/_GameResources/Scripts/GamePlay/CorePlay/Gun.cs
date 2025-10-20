92245572a0ac43ea31b1c10c740f5426d3e18c9b
dathv <dathv@android.vn>
2025-10-20 21:24:14 +0700

    Update

--- Assets/_GameResources/Scripts/GamePlay/CorePlay/Gun.cs
+++ Assets/_GameResources/Scripts/GamePlay/CorePlay/Gun.cs
@@ -153,6 +153,8 @@ namespace ColorBlockCrush
 
         public bool CanPushToConveyor()
         {
+            if (IsMovingToConveyor()) return false; 
+
             if (GunPos == GunPos.ON_CONVEYOR || GunPos == GunPos.TWEEN_SORT) return false;
 
             if (GunPos == GunPos.ON_GUN_BOARD)
@@ -414,7 +416,7 @@ namespace ColorBlockCrush
         Tween moveToConveyorTw;
         Tween moveToSlotTw;
 
-        public void MoveToConeyor(Vector3 endPos, Action callback = null)
+        public void MoveToConeyor(Vector3 endPos, Action callback = null, float delay = 0)
         {
             isFireFirstTime = false;
             Sequence moveToConveyorSq = DOTween.Sequence();
@@ -436,8 +438,14 @@ namespace ColorBlockCrush
             moveToConveyorSq.Join(transform.DOScale(Vector3.one * 0.85f, moveToConveyorDuration));
             moveToConveyorSq.Append(transform.DOPunchScale(Vector3.one * 0.2f, moveToSlotDuration));
             moveToConveyorSq.SetId(this);
+            moveToConveyorSq.SetDelay(delay);
 
         }
+
+        public bool IsMovingToConveyor()
+        {
+            return moveToConveyorTw != null && moveToConveyorTw.IsPlaying();
+        }    
 
         public void MoveToSlot(Vector3 endPos, Action callback = null)
         {
