5979012cffe206cec71fdf49c5896d5e9bccafe7
Some One In VN <dat_hoang@git>
2025-10-21 23:32:48 +0700

    Fx Lock

--- Assets/_GameResources/Scripts/GamePlay/Mechanics/KeyLock/LockObject.cs
+++ Assets/_GameResources/Scripts/GamePlay/Mechanics/KeyLock/LockObject.cs
@@ -8,6 +8,8 @@ namespace ColorBlockCrush
 {
     public class LockObject : ObjectOnGunBoardColumn
     {
+        [SerializeField] private Animator m_animator;
+
         public bool IsResolved { get; private set; }
 
         public void Resolve()
@@ -17,10 +19,15 @@ namespace ColorBlockCrush
             IsResolved = true;
 
             transform.DOScale(0, 0.25f)
+                .OnStart(() =>
+                {
+                    m_animator.Play("LockUnlock");
+                })
                 .OnComplete(() =>
                 {
                     LevelManager.Instance.LevelGame.GunBoardController.ResolveLock(this);
-                });
+                })
+                .SetDelay(0.85f);
         } 
 
         internal void Init(int column)
