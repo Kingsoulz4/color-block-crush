54bb65a928d4d0d0b39d0159339c49e9acfced7f
dathv <dathv@android.vn>
2025-10-22 12:17:58 +0700

    Fix remove on bonus slot

--- Assets/_GameResources/Scripts/GamePlay/CorePlay/Gun.cs
+++ Assets/_GameResources/Scripts/GamePlay/CorePlay/Gun.cs
@@ -365,6 +365,7 @@ namespace ColorBlockCrush
                 }
 
                 LevelManager.Instance.LevelGame.SlotController.RemoveGun(this);
+                LevelManager.Instance.LevelGame.BonusSlotController.RemoveGun(this);
 
                 moveToConveyorTw.Kill();
                 moveSortSlotTw.Kill();
