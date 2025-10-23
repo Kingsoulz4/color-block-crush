3f73757a05bb4a08bc66ecfb378d68e6801b3eff
Tien Dai - Unity Dev Game <daiht@AS.LOCAL>
2025-10-23 18:15:28 +0700

    fix hand booster

--- Assets/_GameResources/Scripts/GamePlay/Booster/HandBooster.cs
+++ Assets/_GameResources/Scripts/GamePlay/Booster/HandBooster.cs
@@ -49,7 +49,6 @@ namespace ColorBlockCrush
             base.CancelBooster();
             IsShowConfirm = false;
             Camera.main.GetComponent<GameCamera>().MoveZ(originCamZ, 0.2f);
-            GameManager.Instance.SetGameState(GameState.Playing);
         }
 
         public override void ActiveBooster()
@@ -72,14 +71,13 @@ namespace ColorBlockCrush
             base.ShowBooster();
             IsShowConfirm = true;
             Camera.main.GetComponent<GameCamera>().MoveZ(zOffetCam, 0.2f);
-            GameManager.Instance.SetGameState(GameState.Paused);
+            LevelController.Instance.ConveyorController.PauseAllTray();
         }
 
         protected override void Done()
         {
             base.Done();
             Camera.main.GetComponent<GameCamera>().MoveZ(originCamZ, 0.2f);
-            GameManager.Instance.SetGameState(GameState.Playing);
         }
     }
 }
